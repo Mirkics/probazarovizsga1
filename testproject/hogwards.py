70ddf91760a1b16988131b658fdaf7b4590f4972
Mirkics <eri_opre@yahoo.co.uk>
2021-08-06 17:31:47 +0200

    created hogwards.py

---
+++ testproject/hogwards.py
@@ -0,0 +1,13 @@
+'''1 Feladat: Hogwards express jegyautomata
+A Hogwards express nemrég rájött, hogy érdemes lenne önkiszolgálóvá tenni a jegykiállító rendszert a hallgatók vasútvonalán.
+Lehet, hogy drágák a baglyok.
+
+Itt találod az önkiszolgáló webes applikáció prototípusát: https://witty-hill-0acfceb03.azurestaticapps.net/hogwards.html
+
+Készíts egy Python python applikációt (akár csak egy darab python fileban) ami selenium-ot használ.
+
+Teszteld le, hogy az általad megadott adatokkal tölti-e ki a jegyet az applikáció.
+(vigyázz, mert elkézpelhető, hogy némely adatokat egynél több helyen is megjeleníti a jegyen az applikáció)
+
+Nem kell negatív tesztesetet készítened. Egy pozitív teszteset teljesen elég lesz.'''
+
