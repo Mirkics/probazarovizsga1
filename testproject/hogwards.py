e93bf1d9ca63046543440a8991c9c64fcf120650
Mirkics <eri_opre@yahoo.co.uk>
2021-08-08 21:51:05 +0200

    AssertionError solved

--- testproject/hogwards.py
+++ testproject/hogwards.py
@@ -27,16 +27,16 @@ try:
     time.sleep(2)
 
     # tesztadatok
-    name = 'Hermione Granger'
-    date = '2021.08.16.'
-    time = '17:25'
+    name = 'HERMIONE GRANGER'
+    date = ('002021', '8', '16')
+    hour = '17:25'
 
 # tesztadatok felvitele
 
     driver.find_element_by_id('passenger').send_keys(name)
     driver.find_element_by_id('departure-date').send_keys(date)
-    driver.find_element_by_id('departure-time').send_keys(time)
-
+    driver.find_element_by_id('departure-time').send_keys(hour)
+    time.sleep(2)
     issue_ticket_button = driver.find_element_by_xpath('//*[@id="issue-ticket"]')
     issue_ticket_button.click()
 
@@ -52,12 +52,14 @@ try:
     print(side_dep_date)
     side_dep_time = driver.find_element_by_id('side-departure-time').text
     print(side_dep_time)
-
+    time.sleep(2)
 # felvitt és megjelenő adatok összehasonlítása
 
-    # assert name == passenger_name
-    # assert date == dep_date + side_dep_date
-    # assert time == dep_time + side_dep_time
+    assert name == passenger_name
+    assert dep_date == '2021-08-16'
+    assert side_dep_date == '2021-08-16'
+    assert dep_time == '05:25PM'
+    assert side_dep_time == '05:25PM'
 
 finally:
     driver.close()
